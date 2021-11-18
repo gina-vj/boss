80fed7c6c79fa8451d873a2d1ec7d75a316c291d
Marcos <marcos.alvarenga@10pines.com>
2021-11-18 03:37:17 -0300

    Typo

--- Gina-Boss/entities/item/types/FaceMask.gd
+++ Gina-Boss/entities/item/types/FaceMask.gd
@@ -3,13 +3,13 @@ class_name FaceMask
 const type: String = "FACE_MASK"
 const protection_points:int = 10
 
-var current_protecion_points:int = 0
+var current_protection_points:int = 0
 
 func _init():
-	current_protecion_points = protection_points
+	current_protection_points = protection_points
 
 func still_useful():
-	return current_protecion_points > 0
+	return current_protection_points > 0
 
 func receive_damage(amount):
-	current_protecion_points -= amount
+	current_protection_points -= amount
