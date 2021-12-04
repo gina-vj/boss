94517158b4b1243e9c4cf613c29b215a5664b3ea
legajo8716 <nelson@nelson.nelson>
2021-12-04 09:06:04 -0300

    refactor barra barbijo

--- Gina-Boss/GUI/ProgressBarbijo.gd
+++ Gina-Boss/GUI/ProgressBarbijo.gd
@@ -7,3 +7,7 @@ func _physics_process(_delta):
 	visible=PlayerData.using_area_protection()
 	if visible:
 		value=PlayerData.area_protection.current_protection_points
+	if value==max_value:
+		$FinalProgressBar.visible=true
+	else:
+		$FinalProgressBar.visible=false
