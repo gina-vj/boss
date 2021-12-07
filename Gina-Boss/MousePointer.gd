0c23ef1585a801b46ee3408ad9be1fb1fa9b862e
agusgs <garciasmithagustin@gmail.com>
2021-12-07 08:29:14 +0100

    mejorita en el mouse pointer

--- Gina-Boss/MousePointer.gd
+++ Gina-Boss/MousePointer.gd
@@ -1,10 +1,7 @@
 extends Node2D
-	
-func _process(delta):
-	if(Bag.has_throwable_items()):
-		$Sprite.position = get_global_mouse_position()
-		$Sprite.visible = true
-		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
-	else:
-		$Sprite.visible = false
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+
+var arrow: Resource = load("res://assets/crosshair.png")
+
+func _ready():
+	Input.set_custom_mouse_cursor(arrow, 0, Vector2(20,20))
+
