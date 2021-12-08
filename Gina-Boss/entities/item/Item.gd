cac4d1dbeee3b8eb13a0d931e8535df86c437947
agusgs <garciasmithagustin@gmail.com>
2021-12-08 18:53:42 +0100

    feedback al levantar cosas

--- Gina-Boss/entities/item/Item.gd
+++ Gina-Boss/entities/item/Item.gd
@@ -6,14 +6,23 @@ export var item_quantity = 0
 export(int, "Alcohol", "Experimental Vaccine", "Face Mask", "Suit") var item_type_index = 0
 
 onready var item_quantity_node = $ItemQuantityBackground/ItemQuantity
+onready var item_quantity_background = $ItemQuantityBackground
 onready var sfx: AudioStreamPlayer2D = $SFX
 onready var collision = $DetectionArea/CollisionShape2D
+onready var pickup_label:Label = $Pickup/PickupLabel
+onready var pickup_tween: Tween = $Pickup/PickupTween
+onready var font = preload("res://entities/item/Item.tres")
+onready var body = $Body
 
 var type = null
+var font_size = 10
 
 func _ready():
+	font.size = font_size
 	type = index_to_type()
 	item_quantity_node.text = str(item_quantity)
+	pickup_label.text = str(item_quantity)
+	pickup_label.visible = false
 
 func index_to_type():
 	return [
@@ -23,11 +32,19 @@ func index_to_type():
 		Suit.type
 	][item_type_index]
 
+func play_pickup_animation():
+	pickup_label.visible = true
+	pickup_tween.interpolate_property(font, "size", font_size, 100, 1)
+	pickup_tween.start()
+	yield(pickup_tween, "tween_all_completed")
 
 func _on_DetectionArea_body_entered(_body):
-	visible = false
+	body.visible = false
+	item_quantity_background.visible = false
 	collision.set_deferred("disabled", true)
-	sfx.play()
 	Bag.add_item(type, item_quantity)
+	sfx.play()
+	play_pickup_animation()
+	
 	yield(sfx, "finished")
 	queue_free()
