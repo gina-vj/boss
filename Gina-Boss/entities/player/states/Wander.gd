109cb36e647ddd65dda588274c53df85bb1c4dc6
agusgs <garciasmithagustin@gmail.com>
2021-12-10 14:25:29 +0100

    gina muertita

--- Gina-Boss/entities/player/states/Wander.gd
+++ Gina-Boss/entities/player/states/Wander.gd
@@ -2,21 +2,7 @@ extends "res://entities/AbstractState.gd"
 
 var level_navigation: Navigation2D
 
-func _ready():
-	yield(get_tree(), "idle_frame")
-	var tree = get_tree()
-	level_navigation = tree.get_nodes_in_group("LevelNavigation")[0]
-	
-func enter():
-	if level_navigation != null:
-		parent.generate_path(level_navigation)
-
 func update(_delta):
-	if (parent.path.size() > 0):
-		parent.navigate()
-		parent._apply_movement()
-	else:
-		emit_signal("finished", "idleDead")
-	
-func exit():
-	parent.path = []
+	parent._handle_dead_movement()
+	parent._apply_movement()
+	parent._handle_deacceleration()
